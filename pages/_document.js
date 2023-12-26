6105fb0f6a71d5b8bd49c63482b077786503e85f
mehul2711 <mehul271102@gmail.com>
2023-12-26 18:56:14 +0530

    :lipstick: Add Favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Image from "next/image";
 
 export default function Document() {
   return (
@@ -9,7 +10,7 @@ export default function Document() {
           name="description"
           content="a software engineering student who loves to code."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/public/img/icx.webp" />
       </Head>
       <body>
         <Main />
